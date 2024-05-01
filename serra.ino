f44488b77796bb4da909637f763cfa132b1e7287
vitoerream <117685977+vitoerream@users.noreply.github.com>
2024-05-01 12:35:44 +0200

    Update serra.ino
    funziona

--- serra.ino
+++ serra.ino
@@ -1,68 +1,99 @@
 #include <WiFiS3.h>
+#include <DHT.h>
+#define DHTPIN 8   //Pin a cui è connesso il sensore
+#define DHTTYPE DHT11   //Tipo di sensore che stiamo utilizzando (DHT22)
+DHT dht(DHTPIN, DHTTYPE); //Inizializza oggetto chiamato "dht", parametri: pin a cui è connesso il sensore, tipo di dht 11/22
+//Variabili
 
-const char ssid[] = "";  // change your network SSID (name)
-const char pass[] = "";   // change your network password (use for WPA, or use as key for WEP)
 
+float hum;  //Variabile in cui verrà inserita la % di umidità
+float temp; //Variabile in cui verrà inserita la temperatura
+String url = "/input.php";
+
+//parametri
+const char ssid[] = "";  // Nome del WiFi (SSD)
+const char pass[] = "";   // Password del WIFI, in caso sia senza password lasciare vuoto -> "";
+const char* host = ""; //Indirizzo IP pubblico (in questo caso dinamico) del server online
+const int httpPort = 80; //post dell'HTTP, in genere si usa la porta 80
+String HTTP_METHOD = "POST";  //POST
+
+//Connessione Access Point
 WiFiClient client;
 int status = WL_IDLE_STATUS;
 
-int HTTP_PORT = 80;
-String HTTP_METHOD = "POST";  // or POST
-char server[] = "";
-
 void setup() {
   Serial.begin(9600);
+  dht.begin();
 
-  // check for the WiFi module:
+  //Verifica Hardware Wifi
   if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
-    // don't continue
+    Serial.println("\n[HARDWARE]->Comunicazione con il modulo fallita!");
+    //Stop
     while (true)
       ;
   }
-
+  //Verifico aggiornamenti del firmware di arduino
   String fv = WiFi.firmwareVersion();
   if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("Please upgrade the firmware");
+    Serial.println("\n[HARDWARE/FIRMWARE]->Aggiorna il Firmware...");
   }
 
-  // attempt to connect to WiFi network:
+  //Tentativo di connessione all'Access Point
   while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    Serial.print("[WIFI]->Tentativo di connessione a:");
+    Serial.print(ssid);
+    //Connessione
     status = WiFi.begin(ssid, pass);
-
-    // wait 10 seconds for connection:
+        Serial.println("[WIFI]->Connessione in corso...");
     delay(10000);
   }
 
-  // print your board's IP address:
-  Serial.print("IP Address: ");
+  //Stampa indirizzo IP Locale
+  Serial.print("[WIFI]->Indirizzo IP Locale: ");
   Serial.println(WiFi.localIP());
-
-  if (client.connect(server, 80)) {
-  		client.println("POST /serra1.html HTTP/1.1");
-  		client.print("Host: ");
-  		client.println(server);
-  		client.println("Content-Type: application/x-www-form-urlencoded");
-  		client.print("temp=");
-  		client.print("242");//dht22_0.readTemperature();
-  		client.print("&umi=");
-  		client.print("473");//dht22_0.readHumidity();
-  		client.println("&chiave=rc23");
   }
 
-    while (client.connected()) {
+
+void loop() {
+  delay(2000);  //Ritardo di 2 secondi.
+  //Leggi i dati e salvali nelle variabili hum e temp
+  hum = dht.readHumidity();
+  temp= dht.readTemperature();
+  //Stampa umidità e temperatura tramite monitor seriale
+  Serial.print("[HARDWARE]->Umidità: ");
+  Serial.print(hum);
+  Serial.print(" %, Temp: ");
+  Serial.print(temp);
+  Serial.println(" Celsius");
+  //verifica stringa payload, in caso di errata lettura nan non verrà caricato per via di uno spazio -> " nan";
+  Serial.print("[PAYLOAD]->Pacchetto dati da inviare -->");
+  String payload = "temp=" + String(temp) + "&umi=" + String(hum) + "&chiave=rc23";
+  Serial.println(payload);
+
+  //Connessione al server
+  if (!client.connect(host, httpPort)) {
+    Serial.println("[ERRORE]->Connessione al server fallita, controlla l'host e la porta...");
+    return;
+  }
+  //POST
+  Serial.println("[CLIENT]->POST in uscita...");
+  client.print("POST " + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Content-Type: application/x-www-form-urlencoded\r\n" +
+               "Content-Length: " + payload.length() + "\r\n" +
+               "Connection: close\r\n\r\n" +
+               payload);
+  Serial.println("[CLIENT]->POST inviata...");
+   while (client.connected()) {
       if (client.available()) {
-        // read an incoming byte from the server and print it to serial monitor:
+        //Leggi i dati dal server
         char c = client.read();
         Serial.print(c);
       }
     }
-
-    // the server's disconnected, stop the client:
+    Serial.println("\n[CLIENT]->Disconnessione dal server...");
+    //Disconnessione client, server in attesa di una nuova connessione (in loop)
     client.stop();
-}
-void loop() {
+    delay(5000);
+
 }
